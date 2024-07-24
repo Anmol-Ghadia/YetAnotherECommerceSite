3ad3c8b2b38c43c63b8236bb7d8f195631fbaa2b
anmol <me@aghadia.com>
2024-07-24 18:27:32 +0530

    updated documentation for TextInput

--- frontend/src/components/basic/TextInput.jsx
+++ frontend/src/components/basic/TextInput.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from 'react';
 import '../../scss/components/basic/TextInput.scss';
 
 // A themed input for this website.
-// {placeholder} is the initial text
+// {placeholder} is the initial text or prompt displayed to user
 // {setFunction} is the function to change state upon input
 // {type} is the input type: 
 //          - 'text' [default]
 //          - 'password'
 //          - 'email'
 // {invalid} is boolean state which notifies the user that input is incorrect
-//           make sure to set it back to false
+//           make sure to set it back to false, contains shake
+//           intended to be used for short period. for example setting true for
+//           2 seconds to get user attention
 // {success} is boolean state which notifies the user that input is correct
 //           make sure to set it back to false
 export default function TextInput({ placeholder, setFunction, type, invalid, success }) {
@@ -26,7 +28,7 @@ export default function TextInput({ placeholder, setFunction, type, invalid, suc
         <label class="theme-text-input-label">
             <input
                 data-invalid={invalid ? '1' : '0'}
-                data-success={success? '1':'0'}
+                data-success={success ? '1' : '0'}
                 data-has-value={inputValue.length != 0 ? '1' : '0'}
                 class="theme-text-input"
                 type={inputType}
