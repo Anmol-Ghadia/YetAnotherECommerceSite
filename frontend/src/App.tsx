dfee88fa34100c3fb149c6ddb5103c3ce882c5ef
anmol <me@aghadia.com>
2024-08-10 12:25:54 +0530

    added typescript and eslint config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,43 @@
-import React from "react";
-
-import {Route,Routes,Outlet} from "react-router-dom"; 
-import LandingPage from "./pages/LandingPage"
-import BrowsePage from "./pages/BrowsePage"
-import ProductPage from "./pages/ProductPage"
-import UserPage from "./pages/UserPage"
-import CartPage from "./pages/CartPage"
-import AuthPage from "./pages/AuthPage";
-import Footer from "./components/general/Footer";
-import Header from "./components/general/Header";
-import LoginPanel from "./components/specific/LoginPanel";
-import RegisterPanel from "./components/specific/RegisterPanel";
-import LoginPanelNew from "./components/specific/LoginPanelNew";
+import { Route, Routes, Outlet } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import BrowsePage from './pages/BrowsePage';
+import ProductPage from './pages/ProductPage';
+import UserPage from './pages/UserPage';
+import CartPage from './pages/CartPage';
+import AuthPage from './pages/AuthPage';
+import Footer from './components/general/Footer';
+import Header from './components/general/Header';
+import LoginPanel from './components/specific/LoginPanel';
+import RegisterPanel from './components/specific/RegisterPanel';
+import LoginPanelNew from './components/specific/LoginPanelNew';
 
 function App() {
-
   const alwaysOnLayout = (
     <>
       <Outlet />
       <Footer />
     </>
-  )
+  );
 
   const headerLayout = (
     <>
       <Header />
       <Outlet />
     </>
-  )
+  );
 
   return (
     <Routes>
-      {/* All have Footer */}
+      {/* all have Footer */}
       <Route element={alwaysOnLayout}>
-        {/* With Header */}
+        {/* with Header */}
         <Route element={headerLayout}>
           <Route path="browse" element={<BrowsePage />} />
-          <Route path="browse/product/:productId"
-            element={<ProductPage />} />
+          <Route path="browse/product/:productId" element={<ProductPage />} />
           <Route path="user" element={<UserPage />} />
           <Route path="user/cart" element={<CartPage />} />
         </Route>
-        {/* Without Header */}
+        {/* without Header */}
         <Route index element={<LandingPage />} />
         <Route path="auth" element={<AuthPage />}>
           <Route index element={<LoginPanel />} />
--- frontend/src/components/basic/ExampleTSComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface props {
-  title: string;
-}
-
-function ExampleTSComponent({ title }: props) {
-  return <div>{title}</div>;
-}
