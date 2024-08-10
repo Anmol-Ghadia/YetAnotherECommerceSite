088de3f41888d388d61de1926e1274ca8a2cc0e3
Anmol Ghadia <me@aghadia.com>
2024-08-10 14:12:13 +0530

    Added Typescript and linting to frontend  (#1)
    * added eslint and prettier
    
    * added typescript
    
    * added typescript and eslint config
    
    * updated CI workflow
    
    * added working directory to workflow
    
    * prettier ignoring public folder
    
    * removed testing workflow trigger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/index.scss'
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import './scss/index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  // </React.StrictMode> 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// if you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
