1992ee8b1bc724d1ccc4eb1c8ead9c91d8630616
anmol <me@aghadia.com>
2024-08-12 16:46:20 -0700

    converted Button to tsx

---
+++ frontend/src/components/basic/Button.tsx
@@ -0,0 +1,30 @@
+import '../../scss/components/basic/Button.scss';
+
+// fill:    boolean that fills the button if true
+// onClick: is a callback without parameters
+// content: content of the button body
+type Props = {
+  fill?: boolean;
+  onClick?: () => void;
+  content?: React.ReactNode;
+};
+
+// a themed button for this website.
+const Button: React.FC<Props> = function abc({
+  fill = false,
+  onClick = () => {},
+  content = '',
+}) {
+  return (
+    <button
+      type="button"
+      data-fill-button={fill ? '1' : '0'}
+      className="theme-button"
+      onClick={onClick}
+    >
+      {content}
+    </button>
+  );
+};
+
+export default Button;
