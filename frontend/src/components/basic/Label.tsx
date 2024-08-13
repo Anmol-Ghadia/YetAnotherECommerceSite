ff628e0a31ddba1e25cbd56b93d98d93231eac7a
anmol <me@aghadia.com>
2024-08-13 00:14:38 -0700

    coverted Label to tsx

---
+++ frontend/src/components/basic/Label.tsx
@@ -0,0 +1,30 @@
+import '../../scss/components/basic/Label.scss';
+
+// content:         the content of the label, could also be jsx
+// center:          boolean for centering the label in available space
+// allowUserSelect: boolean to allow users to select the content
+type Props = {
+  content?: React.ReactNode;
+  center?: boolean;
+  allowUserSelect?: boolean;
+};
+
+// represents a text displaued to user
+const Label: React.FC<Props> = function label({
+  content = '',
+  center = true,
+  allowUserSelect = false,
+}) {
+  const style: React.CSSProperties = {
+    justifyContent: center ? 'center' : 'initial',
+    userSelect: allowUserSelect ? 'text' : 'none',
+  };
+
+  return (
+    <span style={style} className="label">
+      {content}
+    </span>
+  );
+};
+
+export default Label;
